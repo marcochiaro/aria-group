6deae9d8cdfd20c9dc3d9ea6d58aca537d7293b5
Marco Chiaro <mchiaro79@gmail.com>
2023-08-30 22:12:09 -0300

    change main tag for div

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
         {children}
         <FacebookPixel />
-      </main>
+      </div>
       </body>
     </html>
   );
